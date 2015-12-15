77207e204f9f9395b9c887d64d27d10a3d2da1fb
ingeborg ødegård oftedal <ingebode@1x-193-157-214-31.uio.no>
2015-12-15 15:24:06 +0100

    new repo

---
+++ Mathys/Helpers/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Mathys
+//
+//  Created by ingeborg ødegård oftedal on 15/12/15.
+//  Copyright © 2015 ingeborg ødegård oftedal. All rights reserved.
+//
+
+import Foundation
