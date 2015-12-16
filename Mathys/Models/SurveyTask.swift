c3753609dc6df4d037b15d45dd2610c5879c3811
ingeborg ødegård oftedal <ingebode@1x-193-157-214-31.uio.no>
2015-12-16 09:27:36 +0100

    tasks

---
+++ Mathys/Models/SurveyTask.swift
@@ -0,0 +1,27 @@
+//
+//  SurveyTask.swift
+//  Mathys
+//
+//  Created by ingeborg ødegård oftedal on 15/12/15.
+//  Copyright © 2015 ingeborg ødegård oftedal. All rights reserved.
+//
+
+import Foundation
+import ResearchKit
+
+public var SurveyTask: ORKOrderedTask {
+    
+    var steps = [ORKStep]()
+    
+    // INSTRUCTION STEP
+    let instructionStep = ORKInstructionStep(identifier: Identifier.IntroStep.rawValue)
+    instructionStep.title = "SURVEYINSTRUCTIONSTEP_TITLE".localized
+    instructionStep.text = "SURVEYINSTRUCTIONSTEP_TEXT".localized
+    steps += [instructionStep]
+
+    
+    
+
+    return ORKOrderedTask(identifier: Identifier.SurveyTask.rawValue, steps: steps)
+
+}
