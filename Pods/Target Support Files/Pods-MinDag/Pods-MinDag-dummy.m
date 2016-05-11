4013e86839987ae5a0ab03cab23290df62436c65
pmitche <paulpmit@gmail.com>
2016-05-11 15:39:15 +0200

    Removed hard dependency on ResearchKit, use CocoaPods for Alamofire and ResearchKit instead

---
+++ Pods/Target Support Files/Pods-MinDag/Pods-MinDag-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MinDag : NSObject
+@end
+@implementation PodsDummy_Pods_MinDag
+@end
---
+++ Pods/Target Support Files/Pods-MinDag/Pods-MinDag-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MinDagVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MinDagVersionString[];
+
