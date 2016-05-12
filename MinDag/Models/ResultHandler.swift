c0bd287b6b988410311bcf4394bb8c9e4fa36938
pmitche <paulpmit@gmail.com>
2016-05-12 22:22:58 +0200

    Improved ResultHandler with own CSV object

--- MinDag/Models/ResultHandler.swift
+++ MinDag/Models/ResultHandler.swift
@@ -9,69 +9,91 @@
 import Foundation
 import ResearchKit
 
+private class CSV {
+    // Private helper object imitating a CSV file
+    private var headers = [String]()
+    private var fields = [String]()
+    
+    private func addHeader(header: String) {
+        headers.append(header)
+    }
+    
+    private func addHeaders(headers: [String]) {
+        self.headers += headers
+    }
+    
+    private func addField(field: String) {
+        fields.append(field)
+    }
+    
+    private func addFields(fields: [String]) {
+        self.fields += fields
+    }
+}
+
 public class ResultHandler {
     
     private static let respondentID = UserDefaults.objectForKey(UserDefaultKey.UUID)! as! String
     private static let studyID = UserDefaults.objectForKey(UserDefaultKey.StudyID)! as! String
     
     class func createCSVFromResult(result: ORKTaskResult) -> NSData? {
-        let metadata = getMetadata(result)
-        let resultdata = getResultData(result)
+        let metaCSV = getMetadata(result)
+        let resultCSV = getResultData(result)
         
-        let headers = (metadata[0] + resultdata[0]).joinWithSeparator(",")
-        let fields = (metadata[1] + resultdata[1]).joinWithSeparator(",")
+        let headers = (metaCSV.headers + resultCSV.headers).joinWithSeparator(",")
+        let fields = (metaCSV.fields + resultCSV.fields).joinWithSeparator(",")
         
         return "\(headers)\n\(fields)".dataUsingEncoding(NSUTF8StringEncoding)
     }
 
     
-    private class func getMetadata(taskResult: ORKTaskResult) -> [[String]] {
-        let headers = ["Task", "RespondentID", "StudyID", "StartDate", "EndDate", "TotalTime"]
-        let fields = [taskResult.identifier, respondentID, studyID, taskResult.startDate!.toStringDetailed(), taskResult.endDate!.toStringDetailed(), "\(NSInteger(taskResult.endDate!.timeIntervalSinceDate(taskResult.startDate!)))"]
+    private class func getMetadata(taskResult: ORKTaskResult) -> CSV {
+        let csv = CSV()
+        csv.addHeaders(["Task", "RespondentID", "StudyID", "StartDate", "EndDate", "TotalTime"])
+        csv.addFields([taskResult.identifier, respondentID, studyID, taskResult.startDate!.toStringDetailed(), taskResult.endDate!.toStringDetailed(), "\(NSInteger(taskResult.endDate!.timeIntervalSinceDate(taskResult.startDate!)))"])
         
-        return [headers, fields]
+        return csv
     }
     
-    private class func getResultData(taskResult: ORKTaskResult) -> [[String]] {
+    private class func getResultData(taskResult: ORKTaskResult) -> CSV {
         var counter = 1
-        var headers: [String] = []
-        var fields: [String] = []
+        let csv = CSV()
         
         if let stepResults = taskResult.results as? [ORKStepResult] {
             for stepResult in stepResults {
                 for result in stepResult.results! {
                     if let scaleResult = result as? ORKScaleQuestionResult {
-                        headers.append("Question\(counter)")
+                        csv.addHeader("Question\(counter)")
                         counter += 1
                         if let answer = scaleResult.answer {
-                            fields.append("\(answer)")
+                            csv.addField("\(answer)")
                         } else {
-                            fields.append("\(scaleResult.answer)")
+                            csv.addField("\(scaleResult.answer)")
                         }
                     }
                     if let choiceResult = result as? ORKChoiceQuestionResult {
-                        headers.append("Question\(counter)")
+                        csv.addHeader("Question\(counter)")
                         counter += 1
                         if let _ = choiceResult.answer {
-                           fields.append("\(choiceResult.choiceAnswers![0])")
+                            csv.addField("\(choiceResult.choiceAnswers![0])")
                         } else {
-                            fields.append("\(choiceResult.answer)")
+                            csv.addField("\(choiceResult.answer)")
                         }
                     }
                     if let textResult = result as? ORKTextQuestionResult {
-                        headers.append("Question\(counter)")
+                        csv.addHeader("Question\(counter)")
                         counter += 1
                         if let answer = textResult.answer {
-                            fields.append("\(answer)")
+                            csv.addField("\(answer)")
                         } else {
-                            fields.append("\(textResult.answer)")
+                            csv.addField("\(textResult.answer)")
                         }
                     }
                 }
             }
         }
         
-        return [headers, fields]
+        return csv
 
     }
     
