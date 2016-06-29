22f1cbf54a8d09c275338530a5b11c9198b82596
Kien Vu <kienvu@1x-193-157-238-131.uio.no>
2016-06-29 12:13:52 +0200

    Endret slikt at svar på skjema levers til Nettskjema

--- MinDag/Models/Nettskjema.swift
+++ MinDag/Models/Nettskjema.swift
@@ -11,15 +11,59 @@ import Foundation
 import UIKit
 import ResearchKit
 import Alamofire
+import Locksmith
+
+private enum Field: String {
+    case csrf = "NETTSKJEMA_CSRF_PREVENTION"
+    case studyId = "answersAsMap[551950].textAnswer"
+    case appId = "answersAsMap[551951].textAnswer"
+    case date = "answersAsMap[551952].textAnswer"
+    case time = "answersAsMap[551953].textAnswer"
+    case dateTime = "answersAsMap[551954].textAnswer"
+    case hoursSlept = "answersAsMap[551955].answerOptions"
+    case sleepQuality = "answersAsMap[551956].textAnswer"
+}
+
+let hoursSleptOptions = [
+    0: "1217827",
+    1: "1217828",
+    2: "1217829",
+    3: "1217830",
+    4: "1217831",
+    5: "1217832",
+    6: "1217833",
+    7: "1217834",
+    8: "1217835",
+    9: "1217836",
+    10: "1217837",
+    11: "1217838",
+    12: "1217839",
+    13: "1217840",
+    14: "1217841",
+    15: "1217842",
+]
+
+private extension MultipartFormData {
+    func addString(value: String, field: Field) {
+        self.appendBodyPart(data: value.dataUsingEncoding(NSUTF8StringEncoding)!, name: field.rawValue)
+    }
+    
+    func addTwoString(hours: String, quality: String, hoursField: Field, qualityField: Field) {
+        self.appendBodyPart(data: hours.dataUsingEncoding(NSUTF8StringEncoding)!, name: hoursField.rawValue)
+        self.appendBodyPart(data: quality.dataUsingEncoding(NSUTF8StringEncoding)!, name: qualityField.rawValue)
+    }
+    
+    func addFormattedDateTime(time: NSDate, format: String, field: Field) {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = format
+        self.addString(formatter.stringFromDate(time), field: field)
+    }
+}
 
 class Nettskjema {
     private static let pingUrl = "https://nettskjema.uio.no/ping.html"
-    private static let formUrl = "https://nettskjema.uio.no/answer/deliver.json?formId=69862"
-    private static let csrfField = "NETTSKJEMA_CSRF_PREVENTION"
-    private static let uploadField = "answersAsMap[492011].attachment.upload"
-    
+    private static let formUrl = "https://nettskjema.uio.no/answer/deliver.json?formId=73906"
     private static var csrfToken: String?
-    
     
     private class func getCsrfToken(completion: (String?, NSError?) -> Void) -> () {
         Alamofire.request(.GET, pingUrl)
@@ -36,14 +80,18 @@ class Nettskjema {
         }
     }
     
-    private class func post(file: NSData, csrf: String) {
+    private class func post(valueAdder: (MultipartFormData) -> Void, time: NSDate, csrf: String) {
         Alamofire.upload(
             .POST,
             formUrl,
-            multipartFormData: { multipartFormData in
-                let tokenData = csrf.dataUsingEncoding(NSUTF8StringEncoding)
-                multipartFormData.appendBodyPart(data: tokenData!, name: self.csrfField)
-                multipartFormData.appendBodyPart(data: file, name: self.uploadField, fileName: "answer.csv", mimeType: "text/csv")
+            multipartFormData: {
+                $0.addString(csrf, field: Field.csrf)
+                $0.addString(Locksmith.loadDataForUserAccount(Encrypted.account)![Encrypted.studyID]! as! String, field: Field.studyId)
+                $0.addString(UserDefaults.objectForKey(UserDefaultKey.UUID)! as! String, field: Field.appId)
+                $0.addFormattedDateTime(time, format: "yyyy-MM-dd", field: Field.date)
+                $0.addFormattedDateTime(time, format: "HH:mm:ss", field: Field.time)
+                $0.addFormattedDateTime(time, format: "yyyy-MM-dd HH:mm:ss", field: Field.dateTime)
+                valueAdder($0)
             },
             encodingCompletion: { encodingResult in
                 switch encodingResult {
@@ -57,15 +105,25 @@ class Nettskjema {
         })
     }
     
-    class func upload(file: NSData) {
+    private class func submit(using dataAdder: (MultipartFormData) -> Void, time: NSDate) {
         getCsrfToken { (data, error) in
             if let token = data {
-                self.post(file, csrf: token)
+                self.post(dataAdder, time: time, csrf: token)
             } else {
                 NSLog("Failed to get CSRF token with error: \(error!)")
             }
         }
     }
+    
+    private class func submit(sleep value: String, quality: String, time: NSDate) {
+        submit(using: { data in data.addTwoString(value, quality: quality, hoursField: Field.hoursSlept, qualityField: Field.sleepQuality)},
+               time: time)
+    }
+    
+    class func submit(hours: Int, quality: Int, time: NSDate) {
+        submit(sleep: hoursSleptOptions[hours]!, quality: String(quality), time: time)
+    }
+    
 }
 
 
